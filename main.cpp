f1ccf49f5700b341b2bd320fc21de581eda9b8b5
zaira <zairag081@gmail.com>
2024-11-14 14:08:04 -0300

    se sube el menu

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+/*
 struct alumno
 {
     int legajo;
@@ -22,13 +23,88 @@ alumno desencolar(nodo *&fte, nodo *&fin);
 nodo *insertarOrdenado(nodo *&lista, alumno info);
 nodo *buscar(nodo *lista, int legajo);
 nodo *insertarsinRepetir(nodo *&lista, int sucursal);
+*/
+
+// vector
+struct especialidad{
+	int id;
+	char descrpcion [50+1];
+};
+
+
+// structs info
+struct paciente {
+	int id;
+	char nombre[50+1];
+	char apellido[50+1];
+	int edad;
+	char dni [8+1];
+	int telefono;
+};
+struct medico {
+	int id;
+	char nombre[50+1];
+	char apellido[50+1];
+	int matricula;
+	int idEspecialidad;
+	int diasAtencion;
+	int rangoHorario;
+	int tiempoConsulta;
+};
+struct turno {
+	int id;
+	nodo_turnoPaciente *listaTurnoPaciente;
+};
+
+struct infoTurnoPaciente{
+	int id;
+	int hora;
+	int diaSem;
+	int mes;
+	char status;
+	int idPaciente;	
+};
+
+// structs nodos
+struct nodo_turnoPaciente{
+	infoTurnoPaciente info;
+	nodo_turnoPaciente *sgte;
+};
+
+struct nodo_paciente {
+	paciente info;
+	nodo_paciente *sgte;
+};
+struct nodo_medico {
+	medico info;
+	nodo_medico *sgte;
+	
+};
+struct nodo_turno {
+	turno info;
+	nodo_turno *sgte;
+	
+};
+
+// variables globales
+nodo_paciente *listaPacientes = NULL;
+nodo_medico *listaMedicos = NULL;
+nodo_turno *listaTurnos = NULL;
+especialidad vecEspecialidad[20];
+
+// prototipos
+void volcarArchivosaListas();
+void mostrarMenu();
 
 int main()
 {
+	volcarArchivosaListas();
+	mostrarMenu();
 
     return 0;
 }
 
+/*
 alumno pop(nodo *&pila)
 {
     alumno aux;        // aux para guardar la info
@@ -129,3 +205,115 @@ nodo *insertarsinRepetir(nodo *&lista, int sucursal)
     }
     return aux;
 }
+*/
+
+int imprimirMenu(){
+	
+	cout<<endl;
+	cout<<" ** MENU **"<<endl;
+	cout<<endl;
+	cout<<"  ALTA"<<endl;
+	cout<<"    1- NUEVO PACIENTE"<<endl;
+	cout<<"    2- NUEVO TURNO"<<endl;
+	cout<<"    3- NUEVO MEDICO"<<endl;	
+	cout<<endl;
+	cout<<"  ACTUALIZACIONES"<<endl;
+	cout<<"    4- ACTUALIZAR STATUS"<<endl;	
+	cout<<endl;
+	cout<<"  LISTADO"<<endl;
+	cout<<"    5- TURNOS PENDIENTES"<<endl;
+	cout<<"    6- CANTIDAD ATENCIONES EFECTIVAS"<<endl;
+	cout<<"    7- CANCELACIONES"<<endl;
+	cout<<endl;
+	
+	int opc = -1;
+	cout<<"ingrese una opcion: ";
+	cin>>opc;
+	cout<<endl;
+	
+	return 0;
+}
+
+void ejecutarOpcion(int opc){
+	switch(opc){
+		case 1:
+			nuevoPaciente();
+			break;
+		case 2:
+			nuevoTurno();
+			break;
+		case 3:
+			nuevoMedico();
+			break;
+		case 4:
+			actualizarStatus();
+			break;
+		case 5:
+			listarTurnosPendientes();
+			break;
+		case 6:
+			listarAtencionesEfectivas();
+			break;
+		case 7:
+			listarCancelaciones();
+			break;
+		default:
+			break;
+	}
+}
+
+void mostrarMenu(){
+	int opcion = -1;
+	while(opcion != 0){
+		//mostrar menu y recibir opcion
+		opcion = imprimirMenu();
+		//ejecutar la opcion 
+		ejecutarOpcion(opcion);
+	}
+}
+void insertarPacienteOrd(nodo_paciente *nodo){
+	if (listaPacientes == NULL ||
+        nodo->info.id < listaPacientes->info.id) {
+        nodo->sgte = listaPacientes;
+        listaPacientes = nodo;
+    } else {
+        nodo_paciente *q = listaPacientes;
+        while (q->sgte != NULL &&
+               q->sgte->info.id < nodo->info.id) {
+            q = q->sgte;
+        }
+        nodo->sgte = q->sgte;
+        q->sgte = nodo;
+    }
+}
+
+void volcarPacientesaLista(){
+	FILE *f = fopen("PACIENTES","rb");
+	paciente reg ;
+	
+	while(fread(&reg,sizeof(reg),1,f)){
+		//creo un nodo
+		nodo_paciente *nodo = new nodo_paciente();
+		//cargo el nodo con registro
+		nodo->info.id = reg.id;
+		nodo->info.apellido = reg.apellido;
+		nodo->info.dni = reg.dni;
+		nodo->info.edad = reg.edad;
+		nodo->info.nombre = reg.nombre;
+		nodo->info.telefono = reg.telefono;
+		nodo->sgte = NULL;
+		//inserto nodo en la lista
+		insertarPacienteOrd(nodo);
+		}
+	}
+	
+	fclose(f);
+}
+void volcarMedicosaLista(){
+	cout<<"volcarMedicosaLista";
+}
+void volcarArchivosaListas(){
+	volcarPacientesaLista();
+	volcarMedicosaLista();
+}
+
